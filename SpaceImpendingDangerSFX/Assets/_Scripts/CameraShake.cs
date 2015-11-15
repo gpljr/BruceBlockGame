9650ae9affe1d41f39fa8e2a42d5de7754331648
Elliott Mahler <join.together@gmail.com>
2015-11-14 17:36:22 -0800

    added asteroid scene

---
+++ SpaceImpendingDangerSFX/Assets/_Scripts/CameraShake.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour {
+
+    private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = GetComponentInChildren<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Shake(float intensity)
+    {
+        Quaternion initialRotation = mainCamera.transform.localRotation;
+
+        LeanTween.value(gameObject, intensity, 0.0f, intensity).setEase(LeanTweenType.easeInOutQuad).setOnUpdate((float currentIntensity) =>
+        {
+            mainCamera.transform.localPosition = Random.insideUnitSphere * currentIntensity;
+            mainCamera.transform.localRotation = initialRotation * Quaternion.AngleAxis(currentIntensity * 5f, Random.onUnitSphere);
+        });
+    }
+
+    public void Rumble(float intensity)
+    {
+        LeanTween.value(gameObject, intensity / 10.0f, 0.0f, intensity).setEase(LeanTweenType.easeInQuad).setOnUpdate((float currentIntensity) =>
+        {
+            mainCamera.transform.localPosition = Random.insideUnitSphere * currentIntensity;
+        });
+    }
+}
---
+++ SpaceImpendingDangerSFX/Assets/_Scripts/MoveAsteroid.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAsteroid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		LeanTween.moveZ (gameObject, -20f, 6f).setEase(LeanTweenType.easeOutQuad);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
