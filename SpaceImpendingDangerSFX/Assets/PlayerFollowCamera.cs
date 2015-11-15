5f1b7e901d3c431b26548711c86a257d827a93c4
Thomas Lu <thomastianlu@gmail.com>
2015-11-14 19:02:07 -0800

    Committing newest working character controller

---
+++ SpaceImpendingDangerSFX/Assets/PlayerFollowCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFollowCamera : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = _player.transform.position + Vector3.up * 1.5f;
+	}
+}
