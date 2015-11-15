5efe579d62923eaf17cb4ae1073bde363bacc752
Elliott Mahler <join.together@gmail.com>
2015-11-14 18:48:34 -0800

    added explosion scenes

---
+++ SpaceImpendingDangerSFX/Assets/_Scripts/LookAtAsteroidAndStation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtAsteroidAndStation: MonoBehaviour {
+
+	[SerializeField]
+	private Transform asteroid;
+	[SerializeField]
+	private Transform station;
+
+	[SerializeField]
+	private Camera camera;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 centerPoint = (asteroid.position + station.position) / 2f;
+
+		transform.LookAt (centerPoint, Vector3.up);
+
+		float distance = (transform.position - centerPoint).magnitude;
+
+		float rumbleIntensity = Mathf.Max(0f, 1f / distance - 0.05f) / 5f;
+		if (rumbleIntensity > 0f) {
+			CameraRumble (rumbleIntensity);
+		}
+
+	}
+
+	private void CameraRumble(float intensity) {
+		camera.transform.localPosition = Random.insideUnitSphere * intensity;
+		camera.transform.localRotation = Quaternion.AngleAxis (intensity * 5f, Random.onUnitSphere);
+	}
+}
---
+++ SpaceImpendingDangerSFX/Assets/_Scripts/MoveAsteroidToStation.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAsteroidToStation : MonoBehaviour {
+
+	[SerializeField]
+	private Transform station;
+
+	[SerializeField]
+	private Vector3 offset;
+
+	[SerializeField]
+	private GameObject explosionPrefab;
+
+	private float cameraShakeIntensity;
+
+	// Use this for initialization
+	void Start () {
+		LeanTween.rotateAround (station.gameObject, Vector3.forward, 180f, 4f);
+
+		cameraShakeIntensity = .03f;
+
+		LeanTween.move (gameObject, station.position + offset, 4f).setEase(LeanTweenType.easeInQuad).setOnComplete(() => {
+
+			Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+			LeanTween.value(gameObject, 4f, 0f, 2f).setOnUpdate((float valu) => cameraShakeIntensity = valu).setEase(LeanTweenType.easeOutCirc);
+		});
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		CameraRumble (cameraShakeIntensity);
+	}
+
+	private void CameraRumble(float intensity) {
+		Camera.main.transform.localPosition = Random.insideUnitSphere * intensity;
+		Camera.main.transform.localRotation = Quaternion.AngleAxis (intensity * 5f, Random.onUnitSphere);
+	}
+
+
+}
