a508bff9d1e4574caa290b63fd30fd495e863a01
Thomas Lu <thomastianlu@gmail.com>
2015-11-14 16:05:40 -0800

    Pushing initial scene changes!

---
+++ SpaceImpendingDangerSFX/Assets/FadeInScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeInScript : MonoBehaviour {
+
+    [SerializeField]
+    private float _fadeSpeed = 0.001f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Renderer>().material.color -= new Color(0, 0, 0, 1f) * _fadeSpeed;
+	}
+}
