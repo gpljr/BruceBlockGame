38efa50f5a75327561793fbf20bf08b4f108435b
Thomas Lu <thomastianlu@gmail.com>
2015-11-14 16:37:14 -0800

    First scene completed!

---
+++ SpaceImpendingDangerSFX/Assets/ChangeCamScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeCamScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _newOnCamera;
+    [SerializeField]
+    private GameObject _newOffCamera;
+
+	// Use this for initialization
+	void Start () {
+        _newOnCamera.SetActive(true);
+        _newOffCamera.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- SpaceImpendingDangerSFX/Assets/FadeInScript.cs
+++ SpaceImpendingDangerSFX/Assets/FadeInScript.cs
@@ -6,13 +6,20 @@ public class FadeInScript : MonoBehaviour {
     [SerializeField]
     private float _fadeSpeed = 0.001f;
 
+    [SerializeField]
+    private bool _fadeOut = false;
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        GetComponent<Renderer>().material.color -= new Color(0, 0, 0, 1f) * _fadeSpeed;
+        if (_fadeOut) {
+            GetComponent<Renderer>().material.color += new Color(0, 0, 0, 1f) * _fadeSpeed;
+        }
+        else { 
+            GetComponent<Renderer>().material.color -= new Color(0, 0, 0, 1f) * _fadeSpeed;
+        }
 	}
 }
